f368ec1b0a7606cfa30ce55b56b31989680b001c
Raz1945 <g.sebas.sanchez@gmail.comm>
2023-10-12 22:38:36 -0300

    err picture

--- src/components/Home/components/HomeImg.jsx
+++ src/components/Home/components/HomeImg.jsx
@@ -3,7 +3,7 @@ import { images } from "../../../assets/images"
 export const HomeImg = () => {
   return (
     <picture className='home__picture'>
-      <img className='home__picture-img' src={images.pictureOfMe} alt="Picture of Me" />
+      <img className='home__picture-img' src={images.MyPicture} alt="Picture of Me" />
     </picture>
   )
 }
