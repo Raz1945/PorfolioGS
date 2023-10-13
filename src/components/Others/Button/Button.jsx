b23c0d2799d5935943fe8c57dccded11fdb3942a
Raz1945 <g.sebas.sanchez@gmail.comm>
2023-10-12 22:16:35 -0300

    btn css

--- src/components/Others/Button/Button.jsx
+++ src/components/Others/Button/Button.jsx
@@ -1,12 +1,11 @@
-import './Button.css'
+import './button.css'
 
-const Button = ({ text }) => {
+export const Button = ({ text }) => {
   return (
-
-    <div className='class-btn-container'>
-      <button className="button-62" role="button">{text}</button>
-    </div>
+    <>
+      <div className='class-btn-container'>
+        <button className="button-62" role="button">{text}</button>
+      </div>
+    </>
   )
 }
-
-export default Button
