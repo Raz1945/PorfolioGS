1e763fa9efb659a50e775247130f904cbf2971be
Raz1945 <g.sebas.sanchez@gmail.comm>
2023-10-12 22:07:34 -0300

    update description

---
+++ src/assets/projectsData.js
@@ -0,0 +1,70 @@
+import { images } from './images';
+
+const projectsData = [
+  {
+    imgSrc: images.project_0,
+    alt: 'Testimonials grid section Project',
+    title: 'Testimonials grid section',
+    description:
+      'This challenge is an excellent opportunity for those looking to hone their CSS Grid skills.',
+    linkTo: 'https://raz1945.github.io/Testimonials.github.io/',
+    linkToCode: 'https://github.com/Raz1945/Testimonials.github.io',
+  },
+  {
+    imgSrc: images.project_1,
+    alt: 'Interactive card Project',
+    title: 'Interactive card',
+    description:
+      'This project is a great way to practice DOM manipulation and form validation while also testing my HTML and CSS skills.',
+    linkTo: 'https://raz1945.github.io/Interactive-card.github.io/',
+    linkToCode: 'https://github.com/Raz1945/Interactive-card.github.io',
+  },
+  {
+    imgSrc: images.project_2,
+    alt: 'FyloPage Project',
+    title: 'FyloPage',
+    description:
+      "'Fylo Dark Theme Landing Page' It's another Frontend Mentor challenge with some modifications. It is a perfect training ground to practice Flexbox and/or Grid skills.",
+    linkTo: 'https://fylo-page-vercel-app.vercel.app/',
+    linkToCode: 'https://github.com/Raz1945/FyloPage.vercel.app',
+  },
+  {
+    imgSrc: images.project_3,
+    alt: 'WebSenpai Project',
+    title: 'WebSenpai',
+    description: 'WebSenpai is a project that serves as a playground for honing my skills in DOM manipulation, form validation, and testing my HTML and CSS expertise. It provides an ideal environment to refine my proficiency in Flexbox and/or Grid layouts.',
+    linkTo: 'https://fylo-page-vercel-app.vercel.app/',
+    linkToCode: 'https://github.com/Raz1945/FyloPage.vercel.app',
+  },
+  
+  {
+    imgSrc: images.project_4,
+    alt: 'TodoApp Project',
+    title: 'TodoApp',
+    description:
+      "In this project, you can utilize the power of React, incorporating hooks and the Reducer pattern. You have the ability to:\n\n- Add new tasks to the list.\n- Mark tasks as complete.\n- Delete tasks from the list.\n- Filter tasks based on their status (all, active, completed).\n- Clear all completed tasks.\n- Seamlessly switch between light and dark modes.\n\nAs a bonus, the Reducer pattern has been harnessed to efficiently manage the application's state. Explore and enhance your front-end development skills with this engaging project.",
+    linkTo: 'https://raz1945.github.io/websenpai.github.io/',
+    linkToCode: 'https://github.com/Raz1945/websenpai.github.io',
+  },
+  {
+    imgSrc: images.project_5,
+    alt: 'PokeApp Project',
+    title: 'PokeApp',
+    description:
+      "PokeApp is an exciting collaboration project in which I worked with a colleague. We used Scrum (Kanban) to manage tasks over a period of two weeks.\n\n The web application, developed with React and React Router, connects to an API to provide users with an immersive experience of exploring the Pokémon world.\nTo manage the application's state and side effects, we leveraged React hooks such as useState and useEffect.\n\nIt's important to note that the project is not yet finished, and we are actively working on its completion.",
+
+    linkTo: 'https://pokeapp-lyart.vercel.app/',
+    linkToCode: 'https://github.com/Raz1945/pokeapp',
+  },
+  {
+    imgSrc: images.project_6,
+    alt: 'Audn Project',
+    title: 'Audn',
+    description: '',
+    linkTo: 'https://fylo-page-vercel-app.vercel.app/',
+    linkToCode: 'https://github.com/Raz1945/FyloPage.vercel.app',
+  },
+  // Agregar el resto de los proyectos aquí
+];
+
+export default projectsData;
